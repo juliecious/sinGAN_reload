ec4588b61f3707d34ac48f8a7d33a4761de6cc55
Julie Fang <julie.fang@merckgroup.com>
2021-05-18 15:35:49 +0200

    add generator

---
+++ models.py
@@ -0,0 +1,31 @@
+import os
+import torch
+import torchvision
+import torch.nn as nn
+
+# Vanilla GAN as a starter
+
+def get_generator_block(input_dim, output_dim):
+    return nn.Sequential(
+        nn.Linear(input_dim, output_dim),
+        nn.BatchNorm1d(output_dim),
+        nn.ReLU(inplace=True)
+    )
+
+class Generator(nn.Module, z_dim, im_dim, hidden_dim):
+    def __init__(self):
+        super(Generator, self).__init__()
+        self.gen = nn.Sequential(
+            get_generator_block(z_dim, hidden_dim),
+            get_generator_block(hidden_dim, hidden_dim * 2),
+            get_generator_block(hidden_dim * 2, hidden_dim * 4),
+            get_generator_block(hidden_dim * 4, hidden_dim * 8),
+            nn.Linear(hidden_dim * 8, im_dim),
+            nn.Sigmoid()
+        )
+
+    def forward(self, noise):
+        return self.gen(noise)
+
+    def get_gen(self):
+        return self.gen
