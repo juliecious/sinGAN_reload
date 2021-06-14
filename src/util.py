9caf8b38f69fa9302375e8a0c7a700c7e2568627
Julie Fang <julie.fang@merckgroup.com>
2021-06-14 11:25:23 +0200

    add recon_loss from section 2

--- src/util.py
+++ src/util.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 from torch import autograd
 from torch.autograd import Variable
 
@@ -9,8 +10,18 @@ def move_to_gpu_if_available(t):
     else:  # use CPU
         return t.to(torch.device('cpu'))
 
+def calculate_reconstruction_loss(recon_image, real_image):
+    """
+    Calcuate root mean squared error bewteen reconstructed image and the real image in each layer
+    """
+    return torch.sqrt(nn.MSELoss(recon_image, real_image))
 
 def calculate_gradient_penalty(real_images, fake_images, discriminator, lambda_term=10):
+    """
+    Measures the squared difference between the norm of the gradient of the predictions
+    w.r.t. the input images and 1
+    """
+
     # generate a random number epsilon from a uniform distribution U[0,1]
     epsilon = torch.rand(1, 1).expand(real_images.size())
     epsilon = move_to_gpu_if_available(epsilon)
